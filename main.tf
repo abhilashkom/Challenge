13078105e7f661dda7ec2532df2c2a4748076214
saikrishnaburugula <saikrishna.burugula@gmail.com>
2019-04-02 20:15:51 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,26 +13,3 @@ data "aws_vpc" "main" {
   }
 }
 ## query most recent ami id
- data "aws_ami" "linux" {
-   most_recent = true
-
-   filter {
-     name   = "name"
-     values = ["amzn-ami-hvm-*"]
-   }
-
-   filter {
-     name   = "architecture"
-     values = ["x86_64"]
-   }
-
-   filter {
-     name   = "virtualization-type"
-     values = ["hvm"]
-   }
-
-   filter {
-     name   = "root-device-type"
-     values = ["ebs"]
-   }
- }
